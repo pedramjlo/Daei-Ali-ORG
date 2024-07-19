625a89ccbc2a3f24e4ab5c2cda1a4ce607478562
pedramjlo <pedram_sif@tuta.io>
2024-07-19 19:15:18 +0330

    after effect added to list links

--- src/components/menu-groups.jsx
+++ src/components/menu-groups.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../menu-groups.css';
 
-
 const MenuGroups = () => {
+    const [activeIndex, setActiveIndex] = useState(null);
+    const [content, setContent] = useState('');
+
+    const handleClick = (index, newContent, event) => {
+        event.preventDefault();
+        setActiveIndex(index);
+        setContent(newContent);
+    };
+
+    const links = [
+        { label: 'پیتزا', content: 'Pizza Content' },
+        { label: 'برگر', content: 'Burger Content' },
+        { label: 'مینی پیتزا', content: 'Mini Pizza Content' },
+        { label: 'نوشیدنی', content: 'Drinks Content' },
+        { label: 'پیش غذا', content: 'Appetizers Content' }
+    ];
+
     return (
-        <ul className='menu-list'>
-            <li><a href="#">پیتزا</a></li>
-            <li><a href="#">برگر</a></li>
-            <li><a href="#">مینی پیتزا</a></li>
-            <li><a href="#">نوشیدنی</a></li>
-            <li><a href="#">پیش غذا</a></li>
-        </ul>
+        <div>
+            <ul className='menu-list'>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <a 
+                            href="#" 
+                            onClick={(event) => handleClick(index, link.content, event)} 
+                            className={activeIndex === index ? 'active' : ''}
+                        >
+                            {link.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <div className='content'>
+                {content}
+            </div>
+        </div>
     );
-}
-
-
+};
 
 export default MenuGroups;
+ 
