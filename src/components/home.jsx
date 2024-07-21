87cd4189534cb8b910630ad9f485e7fac799df1f
pedramjlo <pedram_sif@tuta.io>
2024-07-22 00:18:13 +0330

    fixed scroll effect

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 import Header from './header';
 import MenuButton from './menu-btn';
@@ -10,55 +11,62 @@ import Footer from './footer';
 
 import '../home.css';
 
-
 export default function Home() {
+  const menuGroupsRef = useRef(null);
+  const scrollOffset = -30; // Adjust this value to set how much you want to scroll upwards
+
+  const scrollToMenuGroups = () => {
+    if (menuGroupsRef.current) {
+      console.log('Scrolling to menu groups');
+      scroll.scrollTo(menuGroupsRef.current.offsetTop + scrollOffset, {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+      });
+    } else {
+      console.log('menuGroupsRef is null');
+    }
+  };
+
   return (
     <div>
       <Header />
       <div className='home-container'>
 
-
         <section className='section1'>
+          <h2 className='welcome-text'>به دایی علی (شعبه جدید) خوش آمدید</h2>
 
-        <h2 className='welcome-text'>به دایی علی (شعبه جدید) خوش آمدید</h2>
+          <div className='description-1'>
+            <p>منوی رستوران را آنلاین مشاهده کنید و انتقادات و پیشنهادات با ارزش خود را برای بهتر شدن ما با ما در میان بگذارید.</p>
+            <p id='branch2'>شعبه سابق دایی علی (کتلت دایی علی) بحرالعلوم ۱۳*</p>
+          </div>
 
-        <div className='description-1'>
-          <p>منوی رستوران را آنلاین مشاهده کنید و انتقادات و  پیشنهادات با ارزش خود را برای بهتر شدن ما با ما در میان بگذارید.  </p>
-          <p id='branch2'>شعبه سابق دایی علی (کتلت دایی علی) بحرالعلوم ۱۳*</p>
-        </div>
+          <div className='home-illust'>
+            <img src="./icons/home-illust.svg" alt="phone illustration" />
+          </div>
 
-        <div className='home-illust'>
-          <img src="./icons/home-illust.svg" alt="phone illustration" />
-        </div>
-
-
-        <div className='buttons-container'> 
-          <MenuButton />
-          <MessageButton />
-        </div>
-
+          <div className='buttons-container'> 
+            <MenuButton onClick={scrollToMenuGroups} />
+            <MessageButton />
+          </div>
         </section>
 
-
         <div className='bounce'>
-            <BouncingArrow loading="lazy" />
+          <BouncingArrow loading="lazy" />
         </div> 
 
-
-       <section className='section2'>
-        <div className='menu-groups'>
+        <section className='section2' ref={menuGroupsRef}>
+          <div className='menu-groups'>
             <MenuGroups loading="lazy" />
           </div>
-       </section>
+        </section>
 
-       <section>
+        <section>
           <HomeImages />
-       </section>
+        </section>
 
-       <Footer />
-
+        <Footer />
       </div>
-      
     </div>
   );
 }
--- src/components/menu-btn.jsx
+++ src/components/menu-btn.jsx
@@ -1,40 +1,25 @@
 import { MdOutlineFastfood } from "react-icons/md";
-
 import '../buttons.css';
 
+export default function MenuButton({ onClick }) {
+  const MenuButtonStyle = {
+    color: "#FF0000",
+    borderRadius: "20px",
+    width: "10rem",
+    height: "2rem",
+  };
 
+  const iconStyle = {
+    color: "#FF0000", 
+    fontSize: "1.2em",
+  };
 
-
-
-
-export default function MenuButton(){
-
-    const MenuButtonStyle = {
-        color: "FF0000",
-        borderRadius: "20px",
-        width: "10rem",
-        height: "2rem",
-    }
-    
-
-    const iconStyle = {
-        color: "#FF0000", 
-        fontSize: "1.2em",
-        
-    };
-
-    return (
-        <button className="menu-button" style={MenuButtonStyle} > 
-
-        <span style={{ color: "#FF0000" }} >
+  return (
+    <button className="menu-button" style={MenuButtonStyle} onClick={onClick}> 
+      <span style={{ color: "#FF0000" }}>
         برو به منو
-        </span>
-
-        <MdOutlineFastfood style={iconStyle}  />
-
-        
-        </button>
-    );
+      </span>
+      <MdOutlineFastfood style={iconStyle} />
+    </button>
+  );
 }
-
-
