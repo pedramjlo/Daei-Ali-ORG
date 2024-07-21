9d8c4940bc4ff74777542e1c7193b45813e89a7e
pedramjlo <pedram_sif@tuta.io>
2024-07-21 05:39:47 +0330

    only burger cards expand

--- src/components/burgerpage.jsx
+++ src/components/burgerpage.jsx
@@ -6,36 +6,36 @@ import Card from './card';
 const BurgerContent = () => {
     return(
         <div>
-            <Card title="شیکاگو" price="240t">
+            <Card title="شیکاگو" price="240t" isExpandable={true}>
                 <p>2 عدد برگر 80 گرمی گوساله - پنیر گودا - سس برگر و سس باربیکیو - پیاز کاراملی</p>
             </Card>
 
-            <Card title="پاریس" price="270t">
+            <Card title="پاریس" price="270t" isExpandable={true}>
             <p>2 عدد برگر 80 گرمی گوساله - پنیر گودا و پنیر ترکیبی - قارچ - سس برگر و سس خردل شیرین</p>
                 
             </Card>
 
-            <Card title="کلورادو" price="299t">
+            <Card title="کلورادو" price="299t" isExpandable={true}>
                 <p>2 عدد برگر 80 گرمی گوساله شنیسل مرغ گریل شده پنیر گودا سس برگر و سس خردل شیرین - پیاز خورد شده</p>
             </Card>
 
-            <Card title="فلوریدا" price="255t">
+            <Card title="فلوریدا" price="255t" isExpandable={true}>
                 <p>2 عدد برگر 80 گرمی گوساله - پنیر گودا - پیاز خورد شده - سس برگر و سس قارچ و سس خردل شیرین</p>
             </Card>
 
-            <Card title="مکزیکانو" price="259t">
+            <Card title="مکزیکانو" price="259t" isExpandable={true}>
                 <p>2 عدد برگر 80 گرمی گوساله - پنیر گودا - هالوپینو - پپرونی - سس برگر و سس چیلی تای - پیاز خورد شده</p>
             </Card>
 
-            <Card title=" میلان" price="257t">
+            <Card title=" میلان" price="257t" isExpandable={true}>
                 <p>2 برگر 80 گرمی گوساله پنیر گورگونزولا - پنیر گودا - گوجه خشک - سس برگر و سس پستو</p>
             </Card>
 
-            <Card title="تگزاس " price="288t">
+            <Card title="تگزاس " price="288t" isExpandable={true}>
                 <p>2 عدد برگر 80 گرمی گوساله بیکن - پنیر گودا - قارچ با سس باربیکیو تفت داده شده - نان مک - سس برگر و سس خامه شیرین</p>
             </Card>
 
-            <Card title="دبل فولی " price="477t">
+            <Card title="دبل فولی " price="477t" isExpandable={true}>
                 <p>4 عدد برگر 80 گرمی گوساله - پنیر گودا 2 عدد - سس برگر و سس خردل شیرین و سس قارج - بیکن کریسپی</p>
             </Card>
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,25 @@
-// Card.js
+// card.jsx
 import React, { useState } from 'react';
 import '../card.css'; 
 
-const Card = ({ title, price, children }) => {
+const Card = ({ title, price, children, isExpandable }) => {
     const [isExpanded, setIsExpanded] = useState(false);
 
     const handleExpandClick = () => {
-        setIsExpanded(!isExpanded);
+        if (isExpandable) {
+            setIsExpanded(!isExpanded);
+        }
     };
 
     return (
-        <div className={`card ${isExpanded ? 'expanded' : ''}`}>
+        <div className={`card ${isExpandable && isExpanded ? 'expanded' : ''}`}>
             <div className="card-header" onClick={handleExpandClick}>
                 <div className="card-title">{title}</div>
                 <div className="card-price">{price}</div>
             </div>
-            {isExpanded && <div style={{direction:"rtl"}} className="card-content">{children}</div>}
+            {(isExpandable && isExpanded) || !isExpandable ? (
+                <div style={{direction:"rtl"}} className="card-content">{children}</div>
+            ) : null}
         </div>
     );
 };
