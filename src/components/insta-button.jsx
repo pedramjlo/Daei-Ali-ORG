f45556af114a0932432bdcdd70e63948d4bce82a
pedramjlo <pedram_sif@tuta.io>
2024-07-25 19:10:16 +0330

    final commit

--- src/components/insta-button.jsx
+++ src/components/insta-button.jsx
@@ -23,6 +23,7 @@ export default function MessageButton() {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         color="#000000"
+        
       >
         <path
           d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
