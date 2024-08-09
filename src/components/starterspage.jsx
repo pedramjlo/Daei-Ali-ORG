8c9709175bb442e54c1d9ef6ea25894678d3c43d
pedramjlo <pedram_sif@tuta.io>
2024-08-09 23:45:24 +0330

    changed prices

--- src/components/starterspage.jsx
+++ src/components/starterspage.jsx
@@ -6,10 +6,10 @@ import '../card.css';
 const StartersContent = () => {
     return(
         <div>
-            <Card title="سیب زمینی ساده" price="80t">
+            <Card title="سیب زمینی ساده" price="95t">
             </Card>
 
-            <Card title="سیب زمینی ویژه" price="155t">
+            <Card title="سیب زمینی ویژه" price="175t">
             </Card>
 
             <Card title="سالاد کلم" price="30t">
